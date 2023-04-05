031aa5e153e9e5a1491a3f1bb427ecf59d4f32a2
BOOMER <127319193+golona73@users.noreply.github.com>
2023-04-05 12:36:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    
+    for (int i = 4; i >= 0; i--) {
+          System.out.println("");
+    
+      
+      for (int j = 0; j <= i; j++) {
+            System.out.print("*");
+             }
+         
+        
+      }
+   }
+}
